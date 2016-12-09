2829b339c5995883487579596de0822b786a345f
DESKTOP-Q6I40VT\RIBOUT Horace <ribouthorace@gmail.com>
2016-12-09 23:57:57 +0100

    Example coroutine : in planet.cs

---
+++ Diplomacy/Assets/Planet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour {
+
+
+    public Vector3 movement = new Vector3(0, 5, 0);
+
+
+    private ParticleSystem _particleSystem;
+
+	// Use this for initialization
+	void Start () {
+        _particleSystem = this.GetComponent<ParticleSystem>();
+        StartCoroutine(Appear(5) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    IEnumerator Appear(int height)
+    {
+        while(this.transform.position.x < height)
+        {
+            this.transform.Translate(movement*Time.deltaTime);
+            yield return new WaitForSeconds(0.1f);
+        }
+        _particleSystem.Play();
+    }
+}
