c47045c7710dae6f959a654285636c4e365f08c0
RIBOUT Horace <ribouthorace@gmail.com>
2016-12-10 19:49:59 +0100

    flux

--- Diplomacy/Assets/Script/Flux.cs
+++ Diplomacy/Assets/Script/Flux.cs
@@ -3,15 +3,27 @@ using System.Collections;
 
 public class Flux : MonoBehaviour {
 
-    private int food;
-    private int iron;
-    private int powr;
+    private Home home_planet;
+    private Vector3 resources;
 
     public void SetResourses(int food, int iron, int powr)
     {
-        this.food = food;
-        this.iron = iron;
-        this.powr = powr;
+        resources.Set(food, iron, powr);
+    }
+
+    public Vector3 GetResources()
+    {
+        return resources;
+    }
+
+    public void SetHomePlanet(Home planet)
+    {
+        home_planet = planet;
+    }
+
+    public Home GetHomePlanet()
+    {
+        return home_planet;
     }
 	// Use this for initialization
 	void Start () {
