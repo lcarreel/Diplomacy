acfe39dd1bdead52d45ac2ef94e16ebdcf3711cd
DESKTOP-Q6I40VT\RIBOUT Horace <pifi77@hotmail.fr>
2016-12-11 01:10:00 +0100

    Zoom + deplacement + BIG planet Space

--- Diplomacy/Assets/Script/GameMecanic/CameraControl.cs
+++ Diplomacy/Assets/Script/GameMecanic/CameraControl.cs
@@ -3,13 +3,85 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
+    private Camera _camera;
+
+    public int maxSize = 20;
+    public int minSize = 4;
+
+    public float cameraSpeed = 5;
+
+    public Vector2 screenBorder = new Vector2(29,16);
+    private Vector2 positionLimit = new Vector2(29,16);
+    private Vector2 stepForBorder;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        _camera = this.GetComponent<Camera>();
+        float zoomGapMinMax = maxSize - minSize;
+        stepForBorder = new Vector2(screenBorder.x/zoomGapMinMax, screenBorder.y / zoomGapMinMax );
+        positionLimit = stepForBorder * (maxSize - Camera.main.orthographicSize);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        float scrollValue = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollValue > 0) // forward
+        {
+            _camera.orthographicSize = Mathf.Max(Camera.main.orthographicSize - 1, minSize);
+            float zoomGap = maxSize - Camera.main.orthographicSize;
+            cameraSpeed = zoomGap;
+            positionLimit = stepForBorder * zoomGap;
+        } else if(scrollValue < 0)
+        {
+            _camera.orthographicSize = Mathf.Min(Camera.main.orthographicSize + 1, maxSize);
+            float zoomGap = maxSize - Camera.main.orthographicSize;
+            cameraSpeed = zoomGap;
+            positionLimit = stepForBorder * zoomGap;
+        }
+
+        float posX = Input.mousePosition.x;
+        float posY = Input.mousePosition.y;
+        print(posX +", "+posY+" this = "+ this.transform.position+" against limit : "+ positionLimit);
+        if (posX > (Screen.width * 0.9) && this.transform.position.x < positionLimit.x)
+        {
+            this.transform.position += (Vector3)(Vector2.right * Time.deltaTime * cameraSpeed);
+        }
+        if (posX < (Screen.width * 0.1f) && this.transform.position.x > - positionLimit.x)
+        {
+            this.transform.position += (Vector3)(Vector2.left * Time.deltaTime * cameraSpeed);
+        }
+        if (posY > (Screen.height * 0.9f) && this.transform.position.y < positionLimit.y)
+        {
+            this.transform.position += (Vector3)(Vector2.up * Time.deltaTime * cameraSpeed);
+        }
+        if (posY < (Screen.height * 0.1f) && this.transform.position.y > - positionLimit.y)
+        {
+            this.transform.position += (Vector3)(Vector2.down * Time.deltaTime * cameraSpeed);
+        }
+
+        Vector3 corrigedPosition = this.transform.position;
+        if(this.transform.position.x > positionLimit.x)
+        {
+            corrigedPosition.x = positionLimit.x;
+        }
+        if (this.transform.position.x < -positionLimit.x)
+        {
+            corrigedPosition.x = -positionLimit.x;
+        }
+        if (this.transform.position.y > positionLimit.y)
+        {
+            corrigedPosition.y = positionLimit.y;
+        }
+        if (this.transform.position.y < -positionLimit.y)
+        {
+            corrigedPosition.y = -positionLimit.y;
+        }
+        this.transform.position = corrigedPosition;
+    }
+
+
+
+
+
 }
